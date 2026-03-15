e88dee3c5946fa6ca861193bc60da8c974322cb9
David Cruz <david@codebycruz.com>
2026-03-15 00:26:18 -0700

    docs: add blog descriptions to rss data

--- docs/src/pages/rss.xml.ts
+++ docs/src/pages/rss.xml.ts
@@ -15,6 +15,7 @@ export async function GET(context: APIContext) {
 			title: post.data.title,
 			pubDate: post.data.published,
 			author: post.data.author,
+			description: post.data.description,
 			link: `/blog/${post.id}/`,
 		})),
 	});
