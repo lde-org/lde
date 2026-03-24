ed41d1f498366ac73a9a568104080ff3355fbc63
David Cruz <david@codebycruz.com>
2026-03-24 02:12:55 -0700

    feat(ansi): bolding and background coloring

--- packages/ansi/src/init.lua
+++ packages/ansi/src/init.lua
@@ -10,6 +10,15 @@ local ansi = {}
 --- | "cyan"
 --- | "white"
 --- | "gray"
+--- | "bold"
+--- | "bg_red"
+--- | "bg_green"
+--- | "bg_yellow"
+--- | "bg_blue"
+--- | "bg_magenta"
+--- | "bg_cyan"
+--- | "bg_white"
+--- | "bg_gray"
 
 ---@type table<ansi.Color, string>
 local colors = {
@@ -21,7 +30,16 @@ local colors = {
 	magenta = "\27[35m",
 	cyan = "\27[36m",
 	white = "\27[37m",
-	gray = "\27[90m"
+	gray = "\27[90m",
+	bold = "\27[1m",
+	bg_red = "\27[41m",
+	bg_green = "\27[42m",
+	bg_yellow = "\27[43m",
+	bg_blue = "\27[44m",
+	bg_magenta = "\27[45m",
+	bg_cyan = "\27[46m",
+	bg_white = "\27[47m",
+	bg_gray = "\27[100m"
 }
 
 ---@param name ansi.Color
