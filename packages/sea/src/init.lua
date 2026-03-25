e1a3a24ec9e68b59ce479e4fe53ca803cd9d0e66
David Cruz <david@codebycruz.com>
2026-03-25 02:58:18 -0700

    feat(sea): export luajit symbols for macos

--- packages/sea/src/init.lua
+++ packages/sea/src/init.lua
@@ -285,6 +285,8 @@ int main(int argc, char** argv) {
 		args[#args + 1] = "-lm"
 		args[#args + 1] = "-ldl"
 		args[#args + 1] = "-Wl,--export-dynamic" -- expose lua symbols for lua dependencies
+	elseif process.platform == "darwin" then
+		args[#args + 1] = "-Wl,-export_dynamic" -- expose lua symbols for lua dependencies
 	end
 
 	local compiler = env.var("SEA_CC") or "gcc"
