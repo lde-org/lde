3fbdb96679ec222d6cd96d2d8fabaad005c55fad
David Cruz <david@codebycruz.com>
2026-03-24 20:38:34 -0700

    chore: use RUNNER_ARCH instead of processor arch

--- install.ps1
+++ install.ps1
@@ -15,7 +15,7 @@ if ($nightly) {
     $tag = (Invoke-RestMethod "https://api.github.com/repos/$repo/releases/latest").tag_name
 }
 
-$arch = if ($env:PROCESSOR_ARCHITECTURE -eq "ARM64") { "aarch64" } else { "x86-64" }
+$arch = if ($env:RUNNER_ARCH -eq "ARM64") { "aarch64" } else { "x86-64" }
 
 New-Item -ItemType Directory $dir -Force | Out-Null
 Invoke-WebRequest "https://github.com/$repo/releases/download/$tag/lpm-windows-$arch.exe" -OutFile $bin
