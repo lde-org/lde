2747c9421f35e12d27022c1fd071d4624737440a
David Cruz <david@codebycruz.com>
2026-03-24 21:23:27 -0700

    chore: remove debug write-host calls

--- install.ps1
+++ install.ps1
@@ -7,10 +7,6 @@ $nightly = $args -contains "--nightly"
 $versionIndex = [array]::IndexOf($args, "--version")
 $version = if ($versionIndex -ge 0) { $args[$versionIndex + 1] } else { $null }
 
-Write-Host "args: $args"
-Write-Host "nightly: $nightly"
-Write-Host "version: $version"
-
 if ($nightly) {
     $tag = "nightly"
 } elseif ($version) {
@@ -22,7 +18,6 @@ if ($nightly) {
 $arch = if ($env:RUNNER_ARCH -eq "ARM64") { "aarch64" } else { "x86-64" }
 
 New-Item -ItemType Directory $dir -Force | Out-Null
-Write-Host "Downloading: https://github.com/$repo/releases/download/$tag/lpm-windows-$arch.exe"
 Invoke-WebRequest "https://github.com/$repo/releases/download/$tag/lpm-windows-$arch.exe" -OutFile $bin
 
 & $bin --setup
