15d1d6cfb6e880e3cccc14f9bb5c4219fe2fe9c6
David Cruz <david@codebycruz.com>
2026-03-26 16:51:40 -0700

    fix: handle double "built" case and do not print with already built package in lpx

--- packages/lpm-core/src/package/build.lua
+++ packages/lpm-core/src/package/build.lua
@@ -5,30 +5,32 @@ local lpm = require("lpm-core")
 
 ---@type table<lpm.Package, boolean>
 local currentlyBuilding = setmetatable({}, { __mode = "k" })
+---@type table<string, boolean>
+local alreadyBuilt = {}
 
 ---@param package lpm.Package
 ---@param destinationPath string?
 local function buildPackage(package, destinationPath)
-	if currentlyBuilding[package] then
-		return
-	end
+	if currentlyBuilding[package] then return end
 	currentlyBuilding[package] = true
 
 	destinationPath = destinationPath or path.join(package:getModulesDir(), package:getName())
 
 	local target = path.dirname(destinationPath)
-	if not fs.isdir(target) then
-		fs.mkdir(target)
-	end
+	if not fs.isdir(target) then fs.mkdir(target) end
 
 	if package:hasBuildScript() then
-		local p = lpm.verbose and ansi.progress("Building " .. package:getName()) or nil
+		-- Check stamp before showing progress so already-built packages are silent
+		local stampFile = path.join(destinationPath, ".lpm-built")
+		local alreadyDone = alreadyBuilt[destinationPath] or fs.exists(stampFile)
+		local p = (lpm.verbose and not alreadyDone) and ansi.progress("Building " .. package:getName()) or nil
 		local ok, err = package:runBuildScript(destinationPath)
 		if not ok then
 			if p then p:fail("Building " .. package:getName()) end
 			error("Build script failed for package '" .. package:getName() .. "': " .. err)
 		end
 		if p then p:done("Built " .. package:getName()) end
+		alreadyBuilt[destinationPath] = true
 	else
 		fs.mklink(package:getSrcDir(), destinationPath)
 	end
