2dab3d4c8135b9c600b7eacec697f21c76af5aa3
David Cruz <david@codebycruz.com>
2026-04-28 01:19:15 -0700

    feat(lde-build): more build methods and fix assertions

--- packages/lde-build/src/build.lua
+++ packages/lde-build/src/build.lua
@@ -1,6 +1,7 @@
 local curl = require("curl-sys")
 local fs = require("fs")
 local path = require("path")
+local archive = require("archive")
 
 ---@class lde.build.Instance
 ---@field outDir string
@@ -16,7 +17,10 @@ end
 ---@return string
 function Instance:fetch(url)
 	local res, err = curl.get(url)
-	assert(res, "failed to fetch " .. url .. ": " .. err)
+	if not res then
+		error("failed to fetch " .. url .. ": " .. err)
+	end
+
 	return res.body
 end
 
@@ -35,6 +39,55 @@ function Instance:read(rel)
 	local res = fs.read(full)
 	assert(res, "failed to read " .. full)
 	return res
+end
+
+---@param rel string # Relative path at output dir
+---@param dest string # Relative path at output dir
+function Instance:extract(rel, dest)
+	local full = path.join(self.outDir, rel)
+
+	local ok, err = archive.new(full):extract(path.join(self.outDir, dest))
+	if not ok then
+		error("failed to extract " .. full .. ": " .. err)
+	end
+end
+
+---@param rel string # Relative path at output dir
+---@param dest string # Relative path at output dir
+function Instance:copy(rel, dest)
+	local full = path.join(self.outDir, rel)
+
+	local ok, err = fs.copy(full, path.join(self.outDir, dest))
+	if not ok then
+		error("failed to copy " .. full .. ": " .. err)
+	end
+end
+
+---@param rel string # Relative path at output dir
+function Instance:delete(rel)
+	local full = path.join(self.outDir, rel)
+
+	local ok, err = fs.delete(full)
+	if not ok then
+		error("failed to remove " .. full .. ": " .. err)
+	end
+end
+
+---@param rel string # Relative path at output dir
+---@param dest string # Relative path at output dir
+function Instance:move(rel, dest)
+	local full = path.join(self.outDir, rel)
+
+	local ok, err = fs.move(full, path.join(self.outDir, dest))
+	if not ok then
+		error("failed to move " .. full .. ": " .. err)
+	end
+end
+
+---@param rel string # Relative path at output dir
+function Instance:exists(rel)
+	local full = path.join(self.outDir, rel)
+	return fs.exists(full)
 end
 
 ---@param cmd string
